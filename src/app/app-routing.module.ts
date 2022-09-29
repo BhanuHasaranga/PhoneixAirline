04e1c8f2e193d8d34339eb794d4cc56cc717c250
BhanuHasaranga <bhanu20139@gmail.com>
2022-09-29 17:46:51 +0530

    user sign up page done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { StaffSignupComponent } from './staff-signup/staff-signup.component';
+import { UserSignupComponent } from './user-signup/user-signup.component';
 
 const routes: Routes = [
-  { path : '', component : LoginComponent}
+  { path : '', component : LoginComponent},
+  { path : 'UserSignup', component : UserSignupComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { StaffSignupComponent } from './staff-signup/staff-signup.component';
+import { UserSignupComponent } from './user-signup/user-signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    StaffSignupComponent,
+    UserSignupComponent
   ],
   imports: [
     BrowserModule,
